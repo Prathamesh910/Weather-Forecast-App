db27be41f72bd8f65cf60b7766212abc5b47db78
Prathamesh910 <prathameshgiradkar910@gmail.com>
2025-11-28 15:54:05 +0530

    Defined OpenWeatherMap API key and base url

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Weather Forecast App is running...");
+// console.log("Weather Forecast App is running...");
+
+const API_KEY = "253527e6bb138d2127973c8361ebda7b";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
